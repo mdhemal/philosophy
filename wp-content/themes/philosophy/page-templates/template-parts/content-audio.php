77b01e3857ba1945a3181c1192f99cc8ef593f7d
mdhemalakhand <mdhemalakanda199@gmail.com>
2021-11-27 20:43:13 +0600

    updated

--- wp-content/themes/philosophy/page-templates/template-parts/content-audio.php
+++ wp-content/themes/philosophy/page-templates/template-parts/content-audio.php
@@ -1,16 +1,20 @@
+<?php
+    $source_file = "";
+     if(function_exists("the_field")) : 
+        $source_file = get_field("source_file");
+    ?>
+<article class="masonry__brick entry format-audio" data-aos="fade-up">
 
+    <div class="entry__thumb">
+        <a href="single-standard.html" class="entry__thumb-link">
+            <?php the_post_thumbnail( 'large' ); ?>
+        </a>
+        <div class="audio-wrap">
+            <audio id="player" src="<?php echo esc_url($source_file); ?>/assets/media/AirReview-Landmarks-02-ChasingCorporate.mp3" width="100%" height="42" controls="controls"></audio>
+        </div>
+    </div>
 
-                <article class="masonry__brick entry format-audio" data-aos="fade-up">
+    <?php get_template_part('template-parts/common/summery'); ?>
 
-                    <div class="entry__thumb">
-                        <a href="single-standard.html" class="entry__thumb-link">
-                            <?php the_post_thumbnail( 'large' ); ?>
-                        </a>
-                        <div class="audio-wrap">
-                            <audio id="player" src="<?php echo get_template_directory_uri(); ?>/assets/media/AirReview-Landmarks-02-ChasingCorporate.mp3" width="100%" height="42" controls="controls"></audio>
-                        </div>
-                    </div>
-
-                    <?php get_template_part('template-parts/common/summery'); ?>
-
-                </article> <!-- end article -->
+</article> <!-- end article -->
+<?php endif; ?>
--- wp-content/themes/philosophy/page-templates/template-parts/content-video.php
+++ wp-content/themes/philosophy/page-templates/template-parts/content-video.php
@@ -1,12 +1,17 @@
+<?php
+    $source_file = "";
+     if(function_exists("the_field")) : 
+        $source_file = get_field("source_file");
+?>
+<article class="masonry__brick entry format-video" data-aos="fade-up">
+        
+    <div class="entry__thumb video-image">
+         <a href="<?php echo esc_url($source_file); ?>" class="entry__thumb-link">
+            <?php the_post_thumbnail( 'large' ); ?>
+        </a>
+    </div>
 
-                <article class="masonry__brick entry format-video" data-aos="fade-up">
-                        
-                    <div class="entry__thumb video-image">
-                         <a href="single-standard.html" class="entry__thumb-link">
-                            <?php the_post_thumbnail( 'large' ); ?>
-                        </a>
-                    </div>
-    
-                    <?php get_template_part('template-parts/common/summery'); ?>
-    
-                </article> <!-- end article -->
+    <?php get_template_part('template-parts/common/summery'); ?>
+
+</article> <!-- end article -->
+<?php endif; ?>
